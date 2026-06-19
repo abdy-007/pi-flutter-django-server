6b86c6f0b6ed0bbb57d32e90e86c98fa11c5ee80
abdy-007 <agaogly.abdy@gmail.com>
2026-06-19 15:32:49 +0200

    changed database to SQLite

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -75,10 +75,9 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'data' / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
